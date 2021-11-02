11ff185f792ff87e4ffc3e2c41504ec0e0a5dc89
s-bariya-h <bariya301@gmail.com>
2021-11-02 03:25:27 +0100

    Add Task 2 tests

--- cypress/config/config.js
+++ cypress/config/config.js
@@ -1,3 +1,5 @@
 export const baseUrl = "https://magazine.trivago.com/";
 export const searchText = "Texas";
 export const searchResultsNumber = 23;
+export const selectArticleWithText =
+  "Mom Knows Best: Family Vacations in Texas";
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -30,4 +30,63 @@ context("Trivago Case Study Challenge", () => {
       .children()
       .should("have.length", `${config.searchResultsNumber}`);
   });
+
+  it("CASE_ID_2.1 : Open 'Mom Knows Best: Family Vacations in Texas' \
+      and verify all featured hotels are in the side list", () => {
+    // Clicking the search icon
+    cy.get("div.search-icon").click();
+    // Typing searchText i.e. 'Texas' in the search bar
+    // of the search menu
+    cy.get('div[id="search"]')
+      .find("input")
+      .click()
+      .type(`${config.searchText}{enter}`);
+    // Finding and clicking the search result containing the text
+    // 'Mom Knows Best: Family Vacations in Texas'
+    cy.get("div.search-results > section > div.row").within(() => {
+      cy.get("div.article-card")
+        .find("h2")
+        .contains(`${config.selectArticleWithText}`)
+        .click();
+    });
+    //Waiting for the webpage to open
+    cy.wait(500);
+
+    // Searching for all the anchor tags containing <strong> tag inside them
+
+    // !!IMPORTANT: This test fails here because there are multiple `post_content divs`
+    // and all with inconsistent child elements
+    cy.get(".post_content > div > .hotel-and-destination-module").each(() => {
+      const text = cy.find("a > strong").invoke("text");
+      cy.get(".table-of-contents").should("contain", text);
+    });
+  });
+
+  it("CASE_ID_2.2 : Open 'Mom Knows Best: Family Vacations in Texas' \
+      and verify there are no broken links", () => {
+    /// Clicking the search icon
+    cy.get("div.search-icon").click();
+    // Typing searchText i.e. 'Texas' in the search bar
+    // of the search menu
+    cy.get('div[id="search"]')
+      .find("input")
+      .click()
+      .type(`${config.searchText}{enter}`);
+    // Finding and clicking the search result containing the text
+    // 'Mom Knows Best: Family Vacations in Texas'
+    cy.get("div.search-results > section > div.row").within(() => {
+      cy.get("div.article-card")
+        .find("h2")
+        .contains(`${config.selectArticleWithText}`)
+        .click();
+    });
+    //Waiting for the webpage to open
+    cy.wait(500);
+    // Searching for all the anchor tags and
+    // asserting that none of the links have an href attr. equal to #undefined
+    cy.contains("a").each(($a) => {
+      const text = $a.text();
+      expect($a, text).to.not.have.attr("href", "#undefined");
+    });
+  });
 });
